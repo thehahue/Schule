0e7955e27775e052d072d6b63824ec821761b715
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2025-10-10 10:58:51 +0200

    Refactoring extract methods.

--- src/main/java/at/bbrz/Main.java
+++ src/main/java/at/bbrz/Main.java
@@ -8,14 +8,42 @@ import java.time.LocalDate;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Adresse adresseKlaus = new Adresse("Klausstr.", "7a", "2700",
-                "Wiener Neustadt", "Österreich");
-        Mitarbeiter klaus = new Mitarbeiter("Klaus", "Maier",
-                LocalDate.of(1980, 8, 12), adresseKlaus, "4711",
-                2400, false);
-
+        Mitarbeiter klaus = createMitarbeiter();
         System.out.println(klaus);
 
+        Lehrer lehrer1 = createLehrer(); // mit STRG+ALT+m -> extract method aufgerufen
+        Schueler fritzi = createSchueler();
+
+        lehrer1.addSchueler(fritzi);
+
+        System.out.println(lehrer1);
+
+        Chef chef = createChef(lehrer1);
+
+        chef.addMitarbeiter(klaus);
+        chef.addMitarbeiter(lehrer1);
+        //chef.addMitarbeiter(chef); -> Wäre ein Zirkelbezug -> Also Exception werfen
+    }
+
+    private static Chef createChef(Lehrer lehrer1) {
+        return new Chef("Chef", "Supa", LocalDate.of(1950, 1, 13)
+        , lehrer1.getAdresse(), "C0001", 6000);
+    }
+
+    private static Schueler createSchueler() {
+        return new Schueler("Fritzi", "Huber", LocalDate.of(2008, 5, 12),
+                new Adresse("Fritzstr.", "7", "1200", "Wien", "Österreich"), false);
+    }
+
+    private static Mitarbeiter createMitarbeiter() {
+        Adresse adresseKlaus = new Adresse("Klausstr.", "7a", "2700",
+                "Wiener Neustadt", "Österreich");
+        return new Mitarbeiter("Klaus", "Maier",
+                LocalDate.of(1980, 8, 12), adresseKlaus, "4711",
+                2400, false);
+    }
+
+    private static Lehrer createLehrer() {
         Adresse adresseLehrer1 = new Adresse("Wissensweg", "1", "1010",
                 "Wien", "Österreich");
         Lehrer lehrer1= new Lehrer("Willi", "Willswissen",
@@ -24,20 +52,6 @@ public class Main {
         lehrer1.addFach("Mathe");
         lehrer1.addFach("Biologie");
         lehrer1.addFach("Physik");
-
-
-        Schueler fritzi = new Schueler("Fritzi", "Huber", LocalDate.of(2008, 5, 12),
-                new Adresse("Fritzstr.", "7", "1200", "Wien", "Österreich"), false);
-
-        lehrer1.addSchueler(fritzi);
-
-        System.out.println(lehrer1);
-
-        Chef chef = new Chef("Chef", "Supa", LocalDate.of(1950, 1, 13)
-        ,adresseLehrer1, "C0001", 6000);
-
-        chef.addMitarbeiter(klaus);
-        chef.addMitarbeiter(lehrer1);
-        //chef.addMitarbeiter(chef); -> Wäre ein Zirkelbezug -> Also Exception werfen
+        return lehrer1;
     }
 }
