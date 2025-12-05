dc0c9a98aece195ba5930cdd01699b15ddf7347a
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2025-12-05 11:00:05 +0100

    Add Calculator class.

---
+++ src/main/java/at/bbrz/Calculator.java
@@ -0,0 +1,23 @@
+package at.bbrz;
+
+public class Calculator {
+    private int valueA;
+    private int valueB;
+
+    public Calculator(int valueA, int valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+    }
+
+    public int sum() {
+        return valueA + valueB;
+    }
+
+    public int substract() {
+        return valueA - valueB;
+    }
+
+    public int multiply() {
+        return valueA * valueB;
+    }
+}
