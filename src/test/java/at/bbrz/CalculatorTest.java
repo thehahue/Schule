7d3cc37ff0fca6b5cecdfcdf76cd85cc5751ade8
Stefan Hasenhütl <s.hasenhuetl@mediashop-group.com>
2025-12-05 11:25:13 +0100

    Add next tests for calculator.

--- src/test/java/at/bbrz/CalculatorTest.java
+++ src/test/java/at/bbrz/CalculatorTest.java
@@ -27,4 +27,22 @@ class CalculatorTest {
     void calculateMultiplyOfTwoNumbers_shouldReturnCorrectResult() {
         assertEquals(35, calculator.multiply());
     }
+
+    @Test
+    void calculateSumOfTwoNegativeNumbers_shouldReturnCorrectResult() {
+        calculator = new Calculator(-5,-7);
+        assertEquals(-12, calculator.sum());
+    }
+
+    @Test
+    void calculateSubstractOfTwoNegativeNumbers_shouldReturnCorrectResult() {
+        calculator = new Calculator(-5,-7);
+        assertEquals(2, calculator.substract());
+    }
+
+    @Test
+    void calculateMultiplyOfTwoNegativeNumbers_shouldReturnCorrectResult() {
+        calculator = new Calculator(-5,-7);
+        assertEquals(35, calculator.multiply());
+    }
 }
